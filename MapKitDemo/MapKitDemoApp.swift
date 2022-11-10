7597cf74f633f5bd3c1bad461b17f18ef5c22039
Melvin W <melvinhw@me.com>
2022-11-10 09:45:41 +0000

    Initial Commit

---
+++ MapKitDemo/MapKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitDemoApp.swift
+//  MapKitDemo
+//
+//  Created by M W on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
